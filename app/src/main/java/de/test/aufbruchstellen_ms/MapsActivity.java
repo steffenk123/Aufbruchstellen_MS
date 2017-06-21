c3b7b066363d69083605820716a7fd8ccbf6eca2
Sven Beyel <sven.beyel@hs-bochum.de>
2017-06-22 00:18:40 +0200

    Automatisches Laden neuer Polygone in einem Hintergundprozess

--- app/src/main/java/de/test/aufbruchstellen_ms/MapsActivity.java
+++ app/src/main/java/de/test/aufbruchstellen_ms/MapsActivity.java
@@ -3,66 +3,61 @@ package de.test.aufbruchstellen_ms;
 import android.Manifest;
 import android.content.Context;
 import android.content.DialogInterface;
-import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
-import android.graphics.Color;
-import android.net.Uri;
+import android.os.Handler;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
-import android.util.Log;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.Polygon;
-import com.google.android.gms.maps.model.PolygonOptions;
 
-import java.net.URLConnection;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import static android.os.Build.VERSION_CODES.M;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, GoogleMap.OnPolygonClickListener {
 
     // Attributes
     private GoogleMap mMap;
     private ArrayList<Aufbruchstellen> aufbruchstellenList;
-    private TextView infoPolygon;
-    HashMap<Polygon, String> polygonValues;
+    private HashMap<Polygon, String> polygonValues;
+    private boolean mPermissionDenied = false;
+    private UrlConnection urlConnection;
 
+    private ArrayList<Polygon> removeList = new ArrayList<>();
+    private final int POLYGON_UPDATE_INTERVAL = 60000;
+    private final Handler handler = new Handler();
 
     private static final int MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1;
-    private boolean mPermissionDenied = false;
-    UrlConnection urlConnection;
+    private static final String PREFERENCE = "Aufbruchstellen";
+    private static final String KEY_STRING = "Datensatz";
+
+    private TextView infoPolygon;
+    private EditText address;
+    private Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
 
-        infoPolygon = (TextView) findViewById(R.id.infoPolygon);
-
-
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
-
-
-        // urlConnection.delegate = this;
-
+        infoPolygon = (TextView) findViewById(R.id.infoPolygon);
+        context = this;
     }
 
 
@@ -76,47 +71,38 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      * installed Google Play services and returned to the app.
      */
     @Override
-    public void onMapReady(GoogleMap googleMap) {
+    public void onMapReady(final GoogleMap googleMap) {
         mMap = googleMap;
-        // aufbruchstellenList = urlConnection.getAufbruchstellenList();
-        //  Log.d("Test Zeile 70", aufbruchstellenList.toString());
-        urlConnection = new UrlConnection(googleMap);
 
-    /*  urlConnection.execute();
-        while (urlConnection.getResultString().isEmpty()){
-            Log.d("Zeile87", "empty");
+        // Neue URLConnection anlegen und String mit letztem Response aus einer Datei auslesen und daraus Polygone erstellen
+        urlConnection = new UrlConnection(mMap, removeList);
+
+        int updateIntervall = POLYGON_UPDATE_INTERVAL;
+        try {
+            String value = getValue();
+            urlConnection.buildPolygons(value);
+        } catch (Exception ex) {
+            updateIntervall = 0;
+            Toast.makeText(context, "Aufbruchstellen werden geladen!", Toast.LENGTH_SHORT).show();
         }
-        Log.d("Zeile 86", urlConnection.getResultString());
-        save(urlConnection.getResultString());
-*/
 
-        String value = getValue();
-        urlConnection.buildPolygons(value);
-
-        // Nur im Hintergund
-
-
-
-
-
+        // Fuehrt den Hintergund Thread aus um die Polygone zu aktualisieren
+        handler.postDelayed(updatePolygons, updateIntervall);
+        updateIntervall = POLYGON_UPDATE_INTERVAL;
 
         //Add a marker in muenster and move the camera
         LatLng muenster = new LatLng(51.962, 7.626);
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(muenster, 12));
-
         mMap.getUiSettings().setZoomControlsEnabled(true);
 
+        // Clicklistener setzen, um die POlygone Clickbar zu machen
         mMap.setOnPolygonClickListener(this);
 
         // Methodenaufruf für eigenen Standort
         enableMyLocation();
-
-
     }
 
-    /**
-     * dt. Ueberpruefen ob Standortberechtigunbgen erteillt wurden
-     */
+
     public void enableMyLocation() {
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
                 == PackageManager.PERMISSION_GRANTED) {
@@ -136,8 +122,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      * @param grantResults
      * @throws SecurityException
      */
-
-
     public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) throws SecurityException {
 
         if (requestCode == MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION) {
@@ -155,40 +139,68 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                             }
                         }).create().show();
             }
-
         }
     }
 
+    /**
+     * Liefert die Information zu einem gewaehlten Polygon
+     *
+     * @param polygon
+     */
     public void onPolygonClick(Polygon polygon) {
-
         polygonValues = urlConnection.getPolygonValues();
         infoPolygon.setText(polygonValues.get(polygon));
-
-
     }
 
-    public void save(String text){
-       /* SharedPreferences settings;
-        SharedPreferences.Editor editor;
-        settings = context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE);
-        editor = settings.edit();
-
-        editor.putString(PREFS_KEY, text);
-        editor.commit();*/
-        SharedPreferences sharedPreferences;
-        sharedPreferences = getSharedPreferences("MyPrefs",Context.MODE_PRIVATE);
+    /**
+     * Speichert die Aufbruchstellen als String
+     *
+     * @param text
+     */
+    public void save(String text) {
+        SharedPreferences sharedPreferences = getSharedPreferences(PREFERENCE, Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = sharedPreferences.edit();
-        editor.putString("Aufbruchstellen", text);
-        Log.d("Zeile 175", text);
+        editor.putString(KEY_STRING, text);
         editor.commit();
     }
 
-    public String getValue() {
-        SharedPreferences sharedPreferences;
-        String text;
-        sharedPreferences = getSharedPreferences("MyPrefs", Context.MODE_PRIVATE);
-        text = sharedPreferences.getString("Aufbruchstellen",null);
-        Log.d("Zeile 183", text);
-        return text;
+    /**
+     * gibt den gespeciherten String mit den Aufbruchstellen aus
+     *
+     * @return
+     */
+    public String getValue() throws Exception {
+        SharedPreferences sharedPreferences = getSharedPreferences(PREFERENCE, Context.MODE_PRIVATE);
+        return sharedPreferences.getString(KEY_STRING, null);
     }
+
+    /**
+     * Laedt im Hintergund die Aufbruchstellen neu
+     */
+    Runnable updatePolygons = new Runnable() {
+        public void run() {
+            // Zuletzt abgerufene Daten speichern
+            if (!urlConnection.getResultString().isEmpty()) {
+                save(urlConnection.getResultString());
+                Toast.makeText(context, "Aufbruchstellen wurden aktualisiert!", Toast.LENGTH_SHORT).show();
+            }
+
+            removeList = urlConnection.getRemoveList();
+            urlConnection = new UrlConnection(mMap, removeList);
+
+            // Ausfuehren
+            urlConnection.execute();
+            handler.postDelayed(this, POLYGON_UPDATE_INTERVAL);
+        }
+    };
+
+
+   /* public void getGeo(String address) throws Exception {
+        Geocoder geoCoder = new Geocoder(this, Locale.getDefault());
+
+        List<Address> addresses = geoCoder.getFromLocationName(address, 1);
+        infoPolygon.setText(addresses.size());
+
+    }*/
+
 }
